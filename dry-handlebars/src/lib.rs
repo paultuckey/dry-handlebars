1b4e0d84a6b50ab3fd56facc81595360077a1aea
Paul Tuckey <paul@tuckey.org>
2025-12-16 20:22:54 +1300

    progress

--- dry-handlebars/src/lib.rs
+++ dry-handlebars/src/lib.rs
@@ -236,4 +236,98 @@ mod tests {
         }
         assert_eq!(template::test(12.2345f64).render(), "Price: $12.23");
     }
+
+
+    // #[test]
+    // fn test_nesting() {
+    //     let rust = compile("{{#if some}}{{#each some}}Hello {{this}}{{/each}}{{/if}}");
+    //     assert_eq!(
+    //         rust,
+    //         "if self.some.as_bool(){for this_2 in self.some{write!(f, \"Hello {}\", this_2.as_display_html())?;}}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn test_as() {
+    //     let rust = compile(
+    //         "{{#if some}}{{#each some as thing}}Hello {{thing}} {{thing.name}}{{/each}}{{/if}}",
+    //     );
+    //     assert_eq!(
+    //         rust,
+    //         "if self.some.as_bool(){for thing_2 in self.some{write!(f, \"Hello {} {}\", thing_2.as_display_html(), thing_2.name.as_display_html())?;}}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn test_scoping() {
+    //     let rust = compile(
+    //         "{{#with some}}{{#with other}}Hello {{name}} {{../company}} {{/with}}{{/with}}",
+    //     );
+    //     assert_eq!(
+    //         rust,
+    //         "{let this_1 = self.some;{let this_2 = this_1.other;write!(f, \"Hello {} {} \", this_2.name.as_display_html(), this_1.company.as_display_html())?;}}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn test_indexer() {
+    //     let rust = compile(
+    //         "{{#each things}}Hello{{{@index}}}{{#each things}}{{{lookup other @../index}}}{{{@index}}}{{/each}}{{/each}}",
+    //     );
+    //     assert_eq!(
+    //         rust,
+    //         "let mut i_1 = 0;for this_1 in self.things{write!(f, \"Hello{}\", i_1.as_display())?;let mut i_2 = 0;for this_2 in this_1.things{write!(f, \"{}{}\", this_2.other[i_1].as_display(), i_2.as_display())?;i_2+=1;}i_1+=1;}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn test_map() {
+    //     let rust = compile(
+    //         "{{#each things}}Hello{{{@key}}}{{#each @value}}{{#if_some (try_lookup other @../key)}}{{{this}}}{{/if_some}}{{{@value}}}{{/each}}{{/each}}",
+    //     );
+    //     assert_eq!(
+    //         rust,
+    //         "for this_1 in self.things{write!(f, \"Hello{}\", this_1.0.as_display())?;for this_2 in this_1.1{if let Some(this_3) = this_2.other.get(this_1.0){write!(f, \"{}\", this_3.as_display())?;}write!(f, \"{}\", this_2.1.as_display())?;}}"
+    //     );
+    // }
+    //
+    //
+    // #[test]
+    // fn test_subexpression() {
+    //     let rust = compile(
+    //         "{{#each things}}{{#with (lookup ../other @index) as |other|}}{{{../name}}}: {{{other}}}{{/with}}{{/each}}",
+    //     );
+    //     assert_eq!(
+    //         rust,
+    //         "let mut i_1 = 0;for this_1 in self.things{{let other_2 = self.other[i_1];write!(f, \"{}: {}\", this_1.name.as_display(), other_2.as_display())?;}i_1+=1;}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn test_selfless() {
+    //     let rust = Compiler::new(Options{
+    //         root_var_name: None,
+    //         write_var_name: "f",
+    //         variable_types: Default::default(),
+    //     }, make_map()).compile("{{#each things}}{{#with (lookup ../other @index) as |other|}}{{{../name}}}: {{{other}}}{{/with}}{{/each}}").unwrap();
+    //     assert_eq!(
+    //         rust.uses("rusty_handlebars").to_string(),
+    //         "use rusty_handlebars::AsDisplay"
+    //     );
+    //     assert_eq!(
+    //         rust.code,
+    //         "let mut i_1 = 0;for this_1 in things{{let other_2 = other[i_1];write!(f, \"{}: {}\", this_1.name.as_display(), other_2.as_display())?;}i_1+=1;}"
+    //     );
+    // }
+    //
+    // #[test]
+    // fn javascript() {
+    //     let rust = Compiler::new(opts(), make_map()).compile("<script>if (location.href.contains(\"localhost\")){ console.log(\"\\{{{{}}}}\") }</script>").unwrap();
+    //     assert_eq!(rust.uses("rusty_handlebars").to_string(), "");
+    //     assert_eq!(
+    //         rust.code,
+    //         "write!(f, \"<script>if (location.href.contains(\\\"localhost\\\")){{ console.log(\\\"{{{{}}}}\\\") }}</script>\")?;"
+    //     );
+    // }
+
 }
